069f7e1a59415c8cdf897d77b2d51357ed822f44
fatima <fatiimahoseini@gmail.com>
2026-09-19 20:07:24 +0330

    Add docker

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
